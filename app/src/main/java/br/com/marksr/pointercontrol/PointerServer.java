28075093134bd89d44d8e6f1214808dfa7eb1b44
marcos.ribeiro <marksr08@hotmail.com>
2016-02-02 23:36:30 -0200

    update
    Signed-off-by: marcos.ribeiro <marksr08@hotmail.com>

--- app/src/main/java/br/com/marksr/pointercontrol/PointerServer.java
+++ app/src/main/java/br/com/marksr/pointercontrol/PointerServer.java
@@ -127,36 +127,6 @@ public class PointerServer extends Service {
         });
 
         commandsServer.start();
-
-        //this code is for dragging the chat head
-        chatHead.setOnTouchListener(new View.OnTouchListener() {
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = params.x;
-                        initialY = params.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        params.x = initialX
-                                + (int) (event.getRawX() - initialTouchX);
-                        params.y = initialY
-                                + (int) (event.getRawY() - initialTouchY);
-                        windowManager.updateViewLayout(chatHead, params);
-                        return true;
-                }
-                return false;
-            }
-        });
         windowManager.addView(chatHead, params);
     }
 
